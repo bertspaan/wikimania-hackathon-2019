f75f87346cd5f872deade000bbac2437bc029e5e
Matt Miller <thisismattmiller@gmail.com>
2019-08-15 17:44:25 +0200

    updateeee

--- grab_tab.js
+++ grab_tab.js
@@ -2,13 +2,16 @@ const request = require('request')
 
 request.post('https://commons.wikimedia.org/wiki/Special:Export', {
   form: {
-    pages: 'Data:Ncei.noaa.gov/weather/New_York_City.tab'
+    pages: 'Data:Ncei.noaa.gov/weather/New_York_City.tab',
+    curonly: 1
   }
 }, (error, res, body) => {
   if (error) {
     console.error(error)
     return
   }
-  console.log(`statusCode: ${res.statusCode}`)
-  console.log(body)
+  var json = body.match(/<text.*?>(.*)<\/text>/)
+  json = JSON.parse(json[1])
+
+  console.log(json)
 })
